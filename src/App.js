26fc3e2fed19f828999de3c95d2d06431052fe4b
sayma63 <saymasumi106353@gmail.com>
2022-07-03 00:17:16 +0600

    about added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Review from './Pages/Review/Review';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import About from './Pages/About/About';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<About></About>}></Route>
+      </Routes>
       
     </div>
   );
