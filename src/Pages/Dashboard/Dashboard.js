a889578445e3b0edc7065e11fa52135b5f882b20
sayma63 <saymasumi106353@gmail.com>
2022-07-03 13:42:53 +0600

    first line chart added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,10 +1,87 @@
+
 import React from 'react';
+import './Dashboard.css'
+
+import { LineChart, Line, XAxis, YAxis, Legend, CartesianGrid, Tooltip, ResponsiveContainer, } from 'recharts';
+
 
 const Dashboard = () => {
+
+
+
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ];
     return (
         <div>
-            
+            <h1 className='heading'>MONTH WISE SELL</h1>
+
+            <div >
+                <div>
+                    <LineChart
+                        width={700}
+                        height={500}
+                        data={data}
+
+                    >
+
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line dataKey="sell" stroke="#8884d8" />
+
+
+                    </LineChart>
+                </div>
+            </div>
+
+
         </div>
+
+
+
+
+        
+
+
+
     );
 };
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <h1 className='text-color text-center mt-3'>Smart Watch Shop</h1>
         </div>
     );
 };
